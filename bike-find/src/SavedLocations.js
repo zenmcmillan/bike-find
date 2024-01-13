2c605a445c4eaefab3e30ff0fe6a1572df19669c
Zen McMillan <zenmcmillan@gmail.com>
2024-01-13 13:27:56 -0600

    Get the saved locations to show in the UI and apply styling

--- bike-find/src/SavedLocations.js
+++ bike-find/src/SavedLocations.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './SavedLocations.css';
+import SavedLocationsCard from './SavedLocationsCard';
 
 export default function SavedLocations({savedLocations}) {
 
   const savedPlaces = savedLocations.map(places => {
     return (
-      <div>
-        <p>{places.extra.address}</p>
-      </div>
+        <SavedLocationsCard savedPlaces={places.extra.address} />
     )
   })
   return (
     <section>
-      <h1>Saved Locations</h1>
-      <div>{savedPlaces}</div>
+     <h1>Saved Locations</h1>
+     <div className="card-container">{savedPlaces}</div>
     </section>
   );
 }
---
+++ bike-find/src/SavedLocationsCard.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SavedLocations({savedPlaces}) {
+  return (
+    <div className="card">
+      <p>{savedPlaces}</p>
+    </div>
+  )
+}
