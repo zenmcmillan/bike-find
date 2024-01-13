a8b1c15887abd1ba1f94a816a52a8e0a489dd415
Zen McMillan <zenmcmillan@gmail.com>
2024-01-12 19:31:44 -0600

    Style the location detail page

--- bike-find/src/LocationDetails.js
+++ bike-find/src/LocationDetails.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import './LocationDetails.css';
 
-export default function LocationDetails({extra}) {
-  const place = useParams().id
-  console.log(useParams())
-  console.log({extra})
+export default function LocationDetails({locations}) {
+  const id = useParams().id
+  console.log("LOCATIONS", locations)
 
+ const place = locations.find((element) => element.id === id);
+ console.log("PLACE",place)
+
+ if (place) {
   return (
-    <div>
-      <h1>Details</h1>
-      {/* <p>{extra.address}</p> */}
-    </div>
-  )
+    <section className="location-details-container">
+      <div className="location-details-card">
+        <h2>Address: {place.extra.address} </h2>
+        <p>Zip Code: {place.extra.postal_code}</p>
+        <p>Empty Slots: {place.empty_slots}</p>
+        <p>Available Bikes: {place.free_bikes}</p>
+        <button className="save-location-button">Save Location</button>
+      </div>
+    </section>
+  );
+ }
 }
 
