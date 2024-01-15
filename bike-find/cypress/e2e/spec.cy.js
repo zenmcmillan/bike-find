6e6570740df1512fedb247140b1aacec8d36282f
Zen McMillan <zenmcmillan@gmail.com>
2024-01-15 15:24:54 -0600

    Test the userflow of saving addresses and deleting them

--- bike-find/cypress/e2e/spec.cy.js
+++ bike-find/cypress/e2e/spec.cy.js
@@ -12,5 +12,38 @@ describe('template spec', () => {
     cy.get('.saved-locations').contains("Saved Locations")
     cy.get('.locations-title').contains("Locations")
     cy.get('.card-container').children().should('have.length', 40)
+    cy.get('.card-container').first().contains('350 East 52nd Street, New York, NY 10022')
+    cy.get('.card-container').last().contains('305-307 West 50th Street, New York, NY 10019')
+  })
+  it("should take user to a detail page when address is clicked, The UI should update when the location is saved or deleted and we should be able to navigate back to the home page", () => {
+    
+    cy.get('.card').contains('554 West 29th Street, New York, NY 10001').click()
+
+    cy.get(".home-title").contains("BikeFind NYC");
+    cy.get(".home").contains("Home");
+    cy.get(".saved-locations").contains("Saved Locations");
+
+    cy.get('.location-details-card').contains('Address: 554 West 29th Street, New York, NY 10001')
+    cy.get('.location-details-card').contains('Zip Code: 10001')
+    cy.get('.location-details-card').contains('Empty Slots: 12')
+    cy.get(".location-details-card").contains('Available Bikes: 1')
+    cy.get(".location-details-card").contains('Save Location').click()
+    
+    cy.get(".location-details-card").contains("Saved ✔");
+    cy.get(".saved-locations").contains("Saved Locations").click()
+    cy.get(".home").contains("Home").click()
+
+    cy.get(".card").contains("143 W 40th Street").click()
+    cy.get(".location-details-card").contains("Save Location").click()
+    cy.get(".location-details-card").contains("Saved ✔")
+    cy.get(".saved-locations").contains("Saved Locations").click()
+
+    cy.get("h1").contains("Saved Locations")
+    cy.get(".saved-card-container").children().should('have.length', 2)
+    
+    cy.get(".saved-locations-card").contains("143 W 40th Street").parent().find('.delete-button').click()
+
+    cy.get(".saved-card-container").children().should("have.length", 1);
+
   })
 })
